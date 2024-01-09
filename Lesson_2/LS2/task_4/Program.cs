5152276fdb79287a481cd8178d2f6c8a25a9f43d
Sergey Koksharov <SerjRazrab@gmail.com>
2024-01-09 20:14:50 +0300

    create_task_4

---
+++ Lesson_2/LS2/task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 5;
+int[] arr = {2,4,7,3,9};
+int i = 0;
+int max = arr[0];
+
+while( i < n )
+{
+    if(arr[i]>max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+Console.Write(max);
