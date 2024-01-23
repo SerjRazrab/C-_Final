dc8261243d12f6c74bee024f022c42720072fe2a
Sergey Koksharov <SerjRazrab@gmail.com>
2024-01-23 21:11:08 +0300

    create task_2 is LS6

---
+++ Lesson_2/LS6/task_2/Program.cs
@@ -0,0 +1,64 @@
+﻿//Задача2
+//-назовем число интересным если его сумма цифр четная
+//-создать двумерный массив ,состоящий из целых чисел.Вывести на экран интересные элементы массива
+
+int [,] GreateMatrix(int columsCount, int rowCount)
+{
+int[,] matrix = new int[columsCount,rowCount];
+Random rnd = new Random();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(1, 1100);
+    }
+}
+return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+}
+
+int [,] matrix = GreateMatrix(3,4);
+ShowMatrix(matrix);
+
+foreach (int e in matrix)
+{
+    if (isInterestig(e) )
+    {
+        Console.WriteLine(e);
+    }
+
+}
+
+bool isInterestig(int value)
+{
+    int sumOfDigits = GetSumOfDigits(value);
+    if(sumOfDigits % 2 ==0)
+    {
+        return true;
+    }
+        return false;
+}
+
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    while (value>0)
+    {
+        sum = sum + value % 10;
+        value = value / 10;
+    }
+    return sum;
+}
