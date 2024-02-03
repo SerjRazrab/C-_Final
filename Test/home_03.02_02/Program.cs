0cf0b6a501531ad8ffc49a8743139c98dc7f49ec
Sergey Koksharov <SerjRazrab@gmail.com>
2024-02-03 18:35:06 +0300

    create task_home_03.02_02

---
+++ Test/home_03.02_02/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+class Program
+{
+    private static void Main()
+    {
+    
+    string dino = "DinOsAur TiRex Is a MONster";
+    string result = string.Empty;
+
+        for(int i = 0; i < dino.Length;i++)
+        
+            if (char.IsUpper(dino[i]))
+            result += char.ToLower(dino[i]);
+            else
+            result+= char.ToUpper(dino[i]);
+
+            Console.WriteLine(result);
+            Console.ReadKey( true );
+    }
+
+}
+
